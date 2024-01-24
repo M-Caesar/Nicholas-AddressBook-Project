ccd5dd0beda37bb8e64146ae73ee403b5d75dc4f
Nicholas Augustus <na50324@email.vccs.edu>
2024-01-24 12:14:28 -0500

    Header file and implementation ammended

--- addressType.cpp
+++ addressType.cpp
@@ -1,1 +1,57 @@
 #include "addressType.h"
+#include <iostream>
+using namespace std;
+
+void addressType::print() //must be defined in the .cpp file
+{
+	cout << address << endl;
+	cout << city << ", " << state << " " << zipcode << endl;
+
+}
+
+//constructor
+
+addressType::addressType(string newaddress, string newcity,string newstate,int newzipcode)
+{
+	address = newaddress;
+	city = newcity;
+	setState(newstate);
+	setZipcode(newzipcode);
+	//state = newstate;
+	//zipcode = newzipcode;
+}
+
+	void addressType::setState(string newstate)
+	{
+		if (newstate.length() > 2)
+		{
+			state = "XX";
+			cout << "State is invalid" << endl;
+		}
+		else
+			state = newstate;
+	}
+
+	void addressType::setZipcode(int zip)
+	{
+		if (zip < 11111 || zip > 99999)
+		{
+			zipcode = 10000;
+				cout << "Zipcode is invalid" << endl;
+		}
+
+		else
+		zipcode = zip;
+	}
+
+	void addressType::setAddress(string newaddress)
+	{
+		address = newaddress;
+	}
+
+	void addressType::setCity(string newcity)
+	{
+		city = newcity;
+	}
+
+
--- addressType.h
+++ addressType.h
@@ -1,3 +1,4 @@
+// header files are the public face of your code, provide the user with what input that they are able to give you
 #pragma once
 #include <iostream>
 using namespace std;
@@ -5,10 +6,37 @@ using namespace std;
 class addressType
 {
 private:
-	string address;
-	string city;
-	string state;
-	int zipcode;
+	string address = "";  // Inlining
+	string city = "";
+	string state = "XX";
+	int zipcode = 10000;
+public:
+	void print(); //prototype declaration of print
 
+	//preconditions - address and city are supplied, supply state as a valiid postal abbreviation
+	//					supply zipcode as a 5-digit integer > 10000
+	// postcondition - use supplied values for address and city, validate state and zipcode
+	addressType(string= "(Your address)", string ="(Your city)", string ="XX", int = 10000);
+
+	//setState
+	//precondition - supplied value is a valid postal (2 character) state abbreeviation
+	//postcondition - use the value supplied if valid, otherwise use the default ("XX")
+	void setState(string newcity);
+
+	//setZipcode
+	//precondition - supplied value is a 5 digit integer > 10000
+	//postcondition - use the supplied value if valid, otherwise use the default (10000)
+	void setZipcode(int newzipcode);
+	void setAddress(string newaddress);
+	void setCity(string newcity);
+
+	//getters
+	string getState() { return state; }
+	int getZipcode() { return zipcode; }
+	string getAddress() { return address; }
+	string getCity() { return city; }
+
+
+	
 };
 
