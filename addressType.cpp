43bbd3ef16b7d22a209ee0f9f21f8e2da751a9ef
Nicholas Augustus <na50324@email.vccs.edu>
2024-01-24 19:59:06 -0500

    Comments added implementation functions

--- addressType.cpp
+++ addressType.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 using namespace std;
 
+//precondition - all variables are input and stored correctly, if not default values will be print
+//postcondition - correct address, city, state and zip code is printed.
 void addressType::print() //must be defined in the .cpp file
 {
 	cout << address << endl;
@@ -10,17 +12,20 @@ void addressType::print() //must be defined in the .cpp file
 }
 
 //constructor
-
+//precondition - constructor for addressType. Accepts client arguments if valid, if not valid or not input, will produce default values for variable corresponding to argument
+//postcondition - variables are correctly assigned in accordance with provided arguments
 addressType::addressType(string newaddress, string newcity,string newstate,int newzipcode)
 {
 	address = newaddress;
 	city = newcity;
-	setState(newstate);
-	setZipcode(newzipcode);
+	setState(newstate); //calling setter function for user input for a state
+	setZipcode(newzipcode); //calling setter function for user input for a zipcode
 	//state = newstate;
 	//zipcode = newzipcode;
 }
 
+//setter function for setting state. Client provides 2 letter abbreviation for state
+//if client provides string with more than 2 characters, the function will not accept input and return default value XX and error message
 	void addressType::setState(string newstate)
 	{
 		if (newstate.length() > 2)
@@ -32,6 +37,8 @@ addressType::addressType(string newaddress, string newcity,string newstate,int n
 			state = newstate;
 	}
 
+	//setter function for setting zipcode. Client provides valid 5 diget zipcode in the range of 11111 and 99999
+	//if provided zipcode is outside of that range, function will not accept input and return default value of 10000 and error message
 	void addressType::setZipcode(int zip)
 	{
 		if (zip < 11111 || zip > 99999)
@@ -44,11 +51,14 @@ addressType::addressType(string newaddress, string newcity,string newstate,int n
 		zipcode = zip;
 	}
 
+	//setter function for setting client input address
+
 	void addressType::setAddress(string newaddress)
 	{
 		address = newaddress;
 	}
 
+	//setter function for setting client input city
 	void addressType::setCity(string newcity)
 	{
 		city = newcity;
--- addressType.h
+++ addressType.h
@@ -11,6 +11,9 @@ private:
 	string state = "XX";
 	int zipcode = 10000;
 public:
+
+	//precondition - all variables are input and stored correctly, if not default values will be print
+	//postcondition - correct address, city, state and zip code is printed.
 	void print(); //prototype declaration of print
 
 	//preconditions - address and city are supplied, supply state as a valiid postal abbreviation
@@ -18,16 +21,20 @@ public:
 	// postcondition - use supplied values for address and city, validate state and zipcode
 	addressType(string= "(Your address)", string ="(Your city)", string ="XX", int = 10000);
 
-	//setState
 	//precondition - supplied value is a valid postal (2 character) state abbreeviation
 	//postcondition - use the value supplied if valid, otherwise use the default ("XX")
 	void setState(string newcity);
 
-	//setZipcode
-	//precondition - supplied value is a 5 digit integer > 10000
+	//precondition - supplied value is a 5 digit integer > 11111 and <99999
 	//postcondition - use the supplied value if valid, otherwise use the default (10000)
 	void setZipcode(int newzipcode);
+
+	//precondition - supplied value is a string for a new address
+	//postcondition - use the supplied value and address string is updated 
 	void setAddress(string newaddress);
+
+	//precondition - supplied value is a string for a new city
+	//postcondition - use the supplied value and city string is updated
 	void setCity(string newcity);
 
 	//getters
