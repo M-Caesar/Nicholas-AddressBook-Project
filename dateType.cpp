68ba423380191af4d131f5a99179e077c793fedc
Nicholas Augustus <na50324@email.vccs.edu>
2024-01-29 11:56:41 -0500

    Created implementation file

--- dateType.cpp
+++ dateType.cpp
@@ -1,1 +1,90 @@
 #include "dateType.h"
+#include <iostream>
+using namespace std;
+
+	dateType::dateType(int month, int day, int year)
+	{
+		dMonth = month;
+		dDay = day;
+		dYear = year;
+	}
+	bool dateType::isLeapYear(int year)
+	{
+		if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	void dateType::setDate(int month, int day, int year)
+	{
+		bool leap = isLeapYear(year);
+
+		//checking month parameter
+		if (month > 12 || month < 1)
+		{
+			dMonth = 1;
+			cout << "Error: invalid month detected." << endl;
+		}
+		else
+			dMonth = month;
+
+		//checking if day is within proper perameters
+		if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+		{
+			if (day > 31 || day < 1)
+			{
+				dDay = 1;
+				cout << "Error: invalid day detected." << endl;
+			}
+			else
+				dDay = day;
+		}
+		else if (month == 4 || month == 6 || month == 9 || month == 11)
+		{
+			if (day > 30 || day < 1)
+			{
+				dDay = 1;
+				cout << "Error: invalid day detected." << endl;
+			}
+			else
+				dDay = day;
+		}
+		else if (month == 2 && leap == true)
+		{
+			if (day > 29 || day < 1)
+			{
+				dDay = 1;
+				cout << "Error: invalid day detected." << endl;
+			}
+			else
+				dDay = day;
+		}
+		else if (day > 28 && leap == false)
+		{
+			if (day > 28 || day < 1)
+			{
+				dDay = 1;
+				cout << "Error invalid day detected." << endl;
+			}
+			else
+				dDay = day;
+		}
+
+		if (year < 1900)
+		{
+			dYear = 1900;
+			cout << "Error: invalid year detected." << endl;
+		}
+		else
+			dYear = year;
+	}
+	//int dateType::getDay() { return dDay; }
+	//int dateType::getMonth() { return dMonth; }
+	//int dateType::getYear() { return dYear; }
+
+	void dateType::print()
+	{
+		cout << dMonth << "-" << dDay << "-" << dYear << endl;
+	}
--- dateType.h
+++ dateType.h
@@ -1,5 +1,27 @@
 #pragma once
+#include <iostream>
+using namespace std;
 class dateType
 {
+private:
+	int dMonth = 1;
+	int dDay = 1;
+	int dYear = 1900;
+
+public:
+	//constructor
+	dateType(int month = 1, int day = 1, int year = 1900);
+	//function for verifying if given year is leap year
+	bool isLeapYear(int Year);
+	//setter function for month, day and year. in this function you will define logic for perameters outside of specifications
+	void setDate(int month, int day, int year);
+	//getter functions for variables
+	int getDay() { return dDay; }
+	int getMonth() { return dMonth; }
+	int getYear() { return dYear; }
+	//print function
+	void print();
+
 };
+
 
