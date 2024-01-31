bad38afda650946a56fdaa73a0891c198b703567
NICHOLAS AUGUSTUS <nicholasaugustus1@gmail.com>
2024-01-30 19:40:33 -0500

    adjustments made to implementation

--- extPersonType.cpp
+++ extPersonType.cpp
@@ -11,24 +11,25 @@ void extPersonType::setRelationship(string rship)
 
 void extPersonType::print()
 {
-	//personType::print();
+	//personType print();
 	cout << getFirstName() << " " << getLastName() << endl;
-	//addressType::print();
-	//cout << "Birthday: " << dateType::print();
+	//addressType print();
+	cout << "Birthday: " << endl;
+	birthdate.print();//not sure why << is not working
 	cout << "Relationship: " << relationship;
 }
 
 extPersonType::extPersonType(string fname, string lname, int nmonth, int nday, int nyear, string naddress, string ncity, string nstate, int nzipcode, string pnumber, string rship)
 {
-	firstName = fname;
-	personType.lastName = lname;
-	dateType.Month = nmonth;
-	dateType.Day = nday;
-	dateType.Year = nyear;
-	addressType.address = naddress;
-	addressType.city = ncity;
-	addressType.state = nstate;
-	addressType.zipCode = nzipcode;
+	personType firstName = fname;
+	personType lastName = lname;
+	dateType Month = nmonth;
+	dateType Day = nday;
+	dateType Year = nyear;
+	addressType address = naddress;
+	addressType city = ncity;
+	addressType state = nstate;
+	addressType zipCode = nzipcode; //cant change from addressType to int?
 	phoneNumber = pnumber;
 	relationship = rship;
 }
--- extPersonType.h
+++ extPersonType.h
@@ -17,7 +17,7 @@ public:
     void setRelationship(string rship);
     string getPhoneNumber() { return phoneNumber;}
     string getRelationship() { return relationship; }
-    int getBirthMonth() { return birthdate; } //may need to create a function to extract month from birthdate
+    int getBirthMonth() { birthdate.getMonth(); } //may need to create a function to extract month from birthdate
     void print();
 
     extPersonType(string = "", string = "", int = 1, int = 1, int = 1900, string = "", string = "", string = "XX", int = 10000, string = "None", string = "None");
