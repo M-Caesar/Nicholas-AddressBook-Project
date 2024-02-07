bc38b25ee4bd121fa2d1c738d893f2912e889950
NICHOLAS AUGUSTUS <nicholasaugustus1@gmail.com>
2024-02-06 19:38:14 -0500

    Further menu implementation

--- Nicholas-AddressBook-Project.cpp
+++ Nicholas-AddressBook-Project.cpp
@@ -4,27 +4,43 @@
 using namespace std;
 
 
-void showMenu()
+void showMenu(addressBookType menuadd)
 	{
 		int choice;
+		string lastName;
+		string relation;
+		int birthmonth;
+		//addressBookType menuadd;
 		do {
 			cout << "~~~~~~~~~~~~~~~~~Main Menu~~~~~~~~~~~~~~~" << endl << "Please enter a choice:" << endl;
-			cout << "Choice 1" << endl;
-			cout << "Choice 2" << endl;
-			cout << "Choice 3" << endl;
+			cout << "Choice 1: Find Person" << endl;
+			cout << "Choice 2: Find Birth Month" << endl;
+			cout << "Choice 3: Find Relationship" << endl;
 			cout << "Choice 4: exit" << endl;
 			cin >> choice;
 
 			switch (choice)
 			{
 			case 1:
-				cout << " You've chosen Choice 1: " << endl;
+				cout << " You've chosen Choice 1: Find Person " << endl;
+				cout << "Please enter a last name: " << endl;
+				cin >> lastName;
+				cout << "This is the last name " << lastName << endl;
+				menuadd.findPerson(lastName);
 				break;
 			case 2:
-				cout << " You've chosen Choice 2: " << endl;
+				cout << " You've chosen Choice 2: Find Birthday " << endl;
+				cout << "Please enter a birth month: " << endl;
+				cin >> birthmonth;
+				cout << "This is the last name " << birthmonth << endl;
+				menuadd.findBirthday(birthmonth);
 				break;
 			case 3:
-				cout << " You've chosen Choice 3: " << endl;
+				cout << " You've chosen Choice 3: Find Relationship " << endl;
+				cout << "Please enter a relationship type: " << endl;
+				cin >> relation;
+				cout << "This is the last name " << relation << endl;
+				menuadd.findRelations(relation);
 				break;
 			case 4:
 				cout << " You've chosen Choice 4: " << endl;
@@ -53,7 +69,7 @@ int main()
 	cout << "Testing the Print function:" << endl << endl;
 	test.print();
 
-	showMenu();
+	showMenu(test);
 
 };
 
